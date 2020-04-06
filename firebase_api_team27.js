7f8b799162833d8f8e64f7127fed213504b83b28
Andi Hung <chihninghung@gmail.com>
2020-04-05 21:33:55 -0700

    updated metabolism page and bmr.page(blank so far)

--- firebase_api_team27.js
+++ firebase_api_team27.js
@@ -1,26 +1,23 @@
+
         //---------------------------------------------------------------------
         // Your web app's Firebase configuration (9 lines of code)
         // Replace the configuration with YOUR project's API information
         // copied from the firebase console (settings) of your project.
         //---------------------------------------------------------------------
-        var firebaseConfig = { //comes from firebase console -> settings, project settings, scroll down to see config
-            apiKey: "AIzaSyDybbFndzdN_qWTlcC2Hv7zISSOrQe4T6k",
-            authDomain: "march05-20fc6.firebaseapp.com",
-            databaseURL: "https://march05-20fc6.firebaseio.com",
-            projectId: "march05-20fc6",
-            storageBucket: "march05-20fc6.appspot.com",
-            messagingSenderId: "939597285390",
-            appId: "1:939597285390:web:e73627aa1298dd75f5945f"
+        var firebaseConfig = {
+          apiKey: "AIzaSyCBQfwiOy_9e2LcENakjXT1t_bH1r4puSo",
+          authDomain: "testing-4a46a.firebaseapp.com",
+          databaseURL: "https://testing-4a46a.firebaseio.com",
+          projectId: "testing-4a46a",
+          storageBucket: "testing-4a46a.appspot.com",
+          messagingSenderId: "1079735765932",
+          appId: "1:1079735765932:web:f7618fb0e728ad6e048809"
         };
-          //------------------------------------------------
-          // Initialize Firebase and Firestore reference
-          // Do not delete!
-          //------------------------------------------------
-          firebase.initializeApp(firebaseConfig);
-          const db = firebase.firestore();
-
-          db.collection("quotes").doc("123")
-            onSnapshot(function (c) {
-              console.log("current data: ", c.data());
-              document.getElementById("data").innerHTML.c.data().message;
-            })
+      
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+      
+        // Get a reference to the database service
+        //const database = firebase.database();
+        const db = firebase.firestore();
+         
--- metabolism.js
+++ metabolism.js
@@ -1,28 +1,96 @@
-
-
-function saveData() {
-    document.getElementById("myform").addEventListener("calculate", function (e) {
+//------User enter information to calculate BMR-------------
+function saveUserData() {
+    document.getElementById("myform").addEventListener("submit", function (e) {
         e.preventDefault();
-        let data = document.getElementById("kg").value //get weight from DOM
-        document.getElementById("input").innerHTML = data;
-        console.log(data);
+        let age = document.getElementById("age").value;
+        let ht = document.getElementById("height").value;
+        let wt = document.getElementById("weight").value;
+        let gen = document.getElementById("gender").value;
+        let act = document.getElementById("act").value;
+        console.log(age);
+        console.log(ht);
+        console.log(wt);
+        console.log(gen);
+        console.log(act);
         firebase.auth().onAuthStateChanged(function (user) {
-            db.collection("users").doc(user.uid).update({
-                "weight": data
+            db.collection("users").doc("c82KHU8AspvFaGZou68v").update({
+                "age" : age,
+                "height" : ht,
+                "weight": wt,
+                "gender": gen,
+                "activitylevel": act
             });
         })
     })
 }
 
-function sayHello() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        name = user.displayName; //get first and last name
-        console.log(name);
-        first = name.split(" ")[0]; //get first name only
-        console.log(first);
-        document.getElementById("name").innerHTML = first + "\'s"; //Elmo's
+//Formula
+//---MALE----BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) + 5
+//---FEMALE-----BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) - 161
+let bmr = 0;
+
+function calculateBMR(){
+    firebase.auth().onAuthStateChanged(function (user){
+        db.collection("users").doc("c82KHU8AspvFaGZou68v")
+        .onSnapshot(function (d) {
+            console.log("Current data: ", d.data());
+            if (d.get("gender") == "male"){
+                bmr = 10 * d.get("weight") + 6.25 * d.get("height") - 5 * d.get("age") + 5;
+                console.log("male", bmr);
+            } else{
+                bmr = 10 * d.get("weight") + 6.25 * d.get("height") - 5 * d.get("age") - 161;
+                console.log("female", bmr);
+               // document.getElementById("totalcalories").innerHTML = bmr;
+            } 
+            console.log(bmr);
+            
+            if(d.get("activitylevel") == "sedentary"){
+                bmr *= 1.2;
+            } else if(d.get("activitylevel") == "somewhat active"){
+                bmr *= 1.375;
+            } else if(d.get("activitylevel") == "active") {
+                bmr *= 1.55;
+            } else if(d.get("activitylevel") == "very active") {
+                bmr *= 1.725;
+            } else {
+                document.getElementById("totalcalories").innerHTML = "invalid";
+            }
+            console.log(bmr);
+            document.getElementById("totalcalories").innerHTML = bmr.toFixed(0);
+        });
     })
 }
+/** 
+function actLevel(){
+    firebase.auth().onAuthStateChanged(function (user){
+        db.collection("users").doc("c82KHU8AspvFaGZou68v")
+        .onSnapshot(function (d) {
+           
+        });
+    })
+}
+*/
 
-sayHello();
-saveData();
+//-----Execute functions -------
+saveUserData();
+
+
+
+/** teting, not working -----------------------
+function saveDat() {
+    document.getElementById("select").addEventListener("submit", function (e) {
+        e.preventDefault();
+        let gender = document.getElementById("gender").value;
+        let actlvl = document.getElementById("act").value;
+        console.log(gender);
+        console.log(actlvl);
+        firebase.auth().onAuthStateChanged(function (user) {
+            db.collection("User name").doc("userkey").update({
+                "gender": gender,
+                "activitylevel": actlvl,
+            });
+        })
+    })
+}
+*/
+//saveData();
